33e6ea3f9d77c1161c06d4df2fb3c66992258d7a
deskach <dzianis.kachanau@gmail.com>
2017-09-24 20:32:59 +1000

    Enabled hot reloading

--- server/main.js
+++ server/main.js
@@ -1,12 +1,39 @@
 const express = require('express');
+// const http = require('http');
+const reload = require('reload');
+const watch = require('watch');
+// const path = require('path')
+// const bodyParser = require('body-parser');
+// const logger = require('morgan');
+
 const app = express();
+
+app.set('port', process.env.PORT || 3000);
+// app.use(logger('dev'));
+// app.use(bodyParser.json()); // Parses json, multi-part (file), url-encoded
 
 app.use('/', express.static('web/'));
 app.use('/bootstrap', express.static('node_modules/bootstrap/dist/'));
 app.use('/popper', express.static('node_modules/popper.js/dist/'));
+app.use('/reload', express.static('node_modules/reload/'));
 
-// app.get('/api', (req, res) => res.send('REST API'));
-// app.get('/api/airports', (req, res) => api.getAirports(req, res));
-// app.get('/api/flights', (req, res) => api.getFlights(req, res));
+// const server = http.createServer(app);
+// server.listen(app.get('port'), function () {
+//     console.log('Web server listening on port ' + app.get('port'))
+// });
 
-app.listen(3000, _ => console.log('Listening on port 3000!'));
+app.listen(app.get('port'), function () {
+    console.log('Web server listening on port ' + app.get('port'))
+});
+
+// Reload code here
+const reloadServer = reload(app);
+
+watch.watchTree(__dirname + "/../web", function (f, curr, prev) {
+    // Fire server-side reload event
+    if (typeof f === 'string') {
+        console.log(`Reloading due to ${f}`);
+
+        reloadServer.reload();
+    }
+});
