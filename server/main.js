9fa2d574ef78e599a8f7ed8fc1908615c83bd45e
deskach <dzianis.kachanau@gmail.com>
2017-09-24 20:00:40 +1000

    Got rid of assets folder

---
+++ server/main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use('/', express.static('web/'));
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist/'));
+app.use('/popper', express.static('node_modules/popper.js/dist/'));
+
+// app.get('/api', (req, res) => res.send('REST API'));
+// app.get('/api/airports', (req, res) => api.getAirports(req, res));
+// app.get('/api/flights', (req, res) => api.getFlights(req, res));
+
+app.listen(3000, _ => console.log('Listening on port 3000!'));
